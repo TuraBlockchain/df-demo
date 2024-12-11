f0093c6a8f9be3bf9f3af8dd8cabfd524bfb0a9a
zhaoyi1998 <1028543855@qq.com>
2024-12-12 02:32:18 +0800

    调整。

--- dfdemo_backend/tags/tasks.py
+++ dfdemo_backend/tags/tasks.py
@@ -2,3 +2,10 @@ import psycopg2
 import threading
 from django.core.cache import cache
 from config.config import DATABASE_NAME, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_PORT
+
+def start_listening():
+    # 初始缓存加载
+    update_cache()
+    # 启动监听线程
+    listener_thread = threading.Thread(target=listen_to_tags_cardkv_changes, daemon=True)
+    listener_thread.start()
