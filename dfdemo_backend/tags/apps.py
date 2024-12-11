b4122ef9441d224cf63c744b695ae5ee616f185f
zhaoyi1998 <1028543855@qq.com>
2024-12-12 02:14:51 +0800

    调整。

--- dfdemo_backend/tags/apps.py
+++ dfdemo_backend/tags/apps.py
@@ -1,12 +1,3 @@
 from django.apps import AppConfig
 import sys
 
-class TagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tags'
-
-    def ready(self):
-        # Only start listening if not in migrate command
-        if 'migrate' not in sys.argv:
-            from .tasks import start_listening
-            start_listening()
